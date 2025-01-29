6c28267ab590d4312d3ea3e3695ddd6cc1bf0252
Ivan Perez <pumi0751@gmail.com>
2025-01-29 15:13:00 +0000

    feat: adding register bussiness

---
+++ src/front/js/component/formularioEmpresa.jsx
@@ -0,0 +1,90 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext.js"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
+
+export const FormularioEmpresas = () => {
+  const {store,actions} = useContext(Context)
+  const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+      name: "",
+      compania: "",
+      email: "",
+      password: "",
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (actions.register(formData)) navigate("/doctors")
+        console.log("User registered:", formData);
+    };
+
+    const handleSignedUp = () => {
+      navigate("/");
+    } 
+    return (
+      <form className="custom-register-form" onSubmit={handleSubmit}>
+        <h1 className="justify-content-center">Registro</h1>
+        <div className="form-group">
+          <label htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="compania">Compañia</label>
+          <input
+            type="text"
+            id="compania"
+            name="compania"
+            value={formData.compania}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">
+            Email <span className="required">*</span>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">
+            Contraseña <span className="required">*</span>
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="custom-register-button" onClick={handleSubmit}>
+          Registrarme
+        </button>
+        <p className="form-footer">
+          ¿Ya tienes cuenta? <a href="/login">Accede aquí</a>
+        </p>
+      </form>
+    );
+  };
+  
